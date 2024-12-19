a1357e69d9e00eaaaf5abbcb7f6fe0aeb8e5abd5
rajeshyo <rajesh95mailbox@gmail.com>
2024-12-19 19:12:49 +0530

    add popup

--- views/script.js
+++ views/script.js
@@ -1,5 +1,6 @@
 const socket = io();
 
+// import * as url from"file!./../image/facebook.png";
 if(navigator.geolocation){
     navigator.geolocation.watchPosition(
         (position) =>{
@@ -28,11 +29,23 @@ const markers = {}
 
 socket.on("receive-location", (data)=>{
     const {id, latitude, longitude} = data;
+    let myPopup = `
+    <h4>Hi Rajesh Saha </h4> <hr/><br/>
+    <a href="https://www.facebook.com/" title="facebook icons"> <img src='https://scontent.fccu11-1.fna.fbcdn.net/v/t39.30808-6/380700650_10162533193146729_2379134611963304810_n.jpg?_nc_cat=1&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=naEUfKPu9CIQ7kNvgFnrpip&_nc_zt=23&_nc_ht=scontent.fccu11-1.fna&_nc_gid=Aq8duOlOhClxvjQSBhWNlPy&oh=00_AYDBNbClQ_EE6qaLettp1pJIyubovMZtA5Maq2mY_swwfg&oe=6769DB7E' alt='maptime logo gif' height='30px' width='30px'/></a>
+
+     <a href="https://www.instagram.com/" title="facebook icons"> <img src='https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg' alt='maptime logo gif' height='30px' width='30px'/></a>
+
+     <a href="/chat" title="facebook icons"> <img src='https://d29fhpw069ctt2.cloudfront.net/icon/image/37750/preview.svg' alt='maptime logo gif' height='30px' width='30px'/></a>
+
+      <a href="/aankhmaro" title="facebook icons"> <img src='https://i.pinimg.com/originals/9f/d7/b0/9fd7b0c0bd4927a39e66c4e43b40b8e6.gif' alt='maptime logo gif' height='40px' width='40px'/></a>
+    `;
+    var customPopup = "<b>My office</b><br/><img src='http://netdna.webdesignerdepot.com/uploads/2014/05/workspace_06_previo.jpg' alt='maptime logo gif' width='150px'/>";
+  
     map.setView([latitude, longitude]);
     if(markers[id]){
         markers[id].setLatLng([latitude,longitude])
     }else{
-        markers[id]= L.marker([latitude,longitude]).addTo(map)
+        markers[id]= L.marker([latitude,longitude]).addTo(map).bindPopup(myPopup).openPopup()
     }
 })
 
